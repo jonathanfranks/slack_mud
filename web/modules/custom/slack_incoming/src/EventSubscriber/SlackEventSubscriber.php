ddab8c68b5e6d79aaaed7dd890b82d6a1b08df3e
Jonathan Franks <jonathan.franks@breaktech.com>
2020-01-03 13:43:11 -0600

    Got a rudimentary home page running

--- web/modules/custom/slack_incoming/src/EventSubscriber/SlackEventSubscriber.php
+++ web/modules/custom/slack_incoming/src/EventSubscriber/SlackEventSubscriber.php
@@ -3,6 +3,8 @@
 namespace Drupal\slack_incoming\EventSubscriber;
 
 use Drupal\slack_incoming\Event\SlackEvent;
+use Drupal\slack_incoming\Service\SlackInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -10,6 +12,23 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  * Class TestStateAlter.
  */
 class SlackEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The Slack service.
+   *
+   * @var \Drupal\slack_incoming\Service\SlackInterface
+   */
+  protected $slack;
+
+  /**
+   * SlackEventSubscriber constructor.
+   *
+   * @param \Drupal\slack_incoming\Service\SlackInterface $slack
+   *   The Slack service.
+   */
+  public function __construct(SlackInterface $slack) {
+    $this->slack = $slack;
+  }
 
   /**
    * {@inheritdoc}
@@ -36,6 +55,32 @@ class SlackEventSubscriber implements EventSubscriberInterface {
       switch ($package['type']) {
         case 'url_verification':
           $event->setResponse(new JsonResponse(['challenge' => $package['challenge']]));
+          break;
+        case 'event_callback':
+          switch ($package['event']['type']) {
+            case 'app_home_opened':
+              $homeBlockContent = "{
+                \"type\": \"home\",
+                \"blocks\": [
+                  {
+                    \"type\": \"section\",
+                    \"text\": {
+                      \"type\": \"mrkdwn\",
+                      \"text\": \"Hello! Use this cool Slack app to play old text-based adventure games!\n\n\"
+                    }
+                  }
+                ]
+              }";
+
+              $this->slack->slackApi('views.publish', 'POST', [
+                'user_id' => $package["event"]["user"],
+                'view' => $homeBlockContent,
+              ]);
+
+
+              break;
+          }
+          break;
       }
     }
   }
